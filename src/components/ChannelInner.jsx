cfef9545b814dac18b8f6479576f32805d818d26
suizaax <suizaax22@gmail.com>
2022-04-12 00:02:32 +0200

    online status

--- src/components/ChannelInner.jsx
+++ src/components/ChannelInner.jsx
@@ -57,8 +57,8 @@ const TeamChannelHeader = ({ setIsEditing }) => {
             <div key={i} className='team-channel-header__name-multi'>
               <Avatar image={user.image} name={user.fullName || user.id} size={32} />
               <p className='team-channel-header__name.user'>{user.fullName || user.id}</p>
-              <p className='team-channel-header__name.user'>{
-                (user.online === false) ? "Offline" : "Online"
+              <p className='team-channel-header__name.status'>{
+                (user.online === false) ? <div className='.team-channel-header__name.status-offline' /> : <div className='.team-channel-header__name.status-online' />
               }</p>
             </div>
           ))}
